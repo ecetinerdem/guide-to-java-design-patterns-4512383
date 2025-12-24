094447dee3a2e1b3b076406b86861d9cbc241e11
Cetin Erdem <e.cetinerdem@gmail.com>
2025-12-24 16:39:15 +0000

    06_04

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,10 +1,25 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
-    Snack getSnack(String snackType) {
-        return null;
+    private static final Map<String, Supplier<Snack>> snackMap = new HashMap<>();
+
+    static {
+        snackMap.put("Drink", Drink::new);
+        snackMap.put("ChocolateBar", ChocolateBar::new);
+        snackMap.put("Chips", Chips::new);
+
     }
 
+    Snack getSnack(String snackType) {
+        if (snackMap.get(snackType) != null) {
+            return snackMap.get(snackType).get();
+        }
+        throw new IllegalArgumentException("Unknown snack type");
+    }
 
 }
