b5a53d742c297a763a9378578b7529eb66ecd0a2
Cetin Erdem <e.cetinerdem@gmail.com>
2025-12-25 16:16:43 +0000

    23_04

--- src/main/java/com/example/Location.java
+++ src/main/java/com/example/Location.java
@@ -1,11 +1,18 @@
 package com.example;
 
-public class Location {
+import java.beans.PropertyChangeSupport;
 
+public class Location {
     private String weatherEvent;
+    private PropertyChangeSupport pChangeSupport = new PropertyChangeSupport(this);
 
     public void setWeatherEvent(String weatherEvent) {
+        pChangeSupport.firePropertyChange("weatherEvent", this.weatherEvent, weatherEvent);
         this.weatherEvent = weatherEvent;
+    }
+
+    public void addPropertyChangeListener(WeatherUpdates weatherUpdates) {
+        pChangeSupport.addPropertyChangeListener(weatherUpdates);
     }
 
 }
