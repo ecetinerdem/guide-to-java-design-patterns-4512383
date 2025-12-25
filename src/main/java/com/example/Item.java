4bef837513281fc992c0516321b1d0ce42487269
Cetin Erdem <e.cetinerdem@gmail.com>
2025-12-25 15:58:22 +0000

    22_03

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -3,6 +3,7 @@ package com.example;
 public class Item {
 
     private int price;
+    private ItemState state = new ItemState(price);
 
     public Item(int price) {
         this.price = price;
@@ -14,6 +15,14 @@ public class Item {
 
     public int getPrice() {
         return price;
+    }
+
+    public void save() {
+        state.setState(price);
+    }
+
+    public void undo() {
+        price = state.getState();
     }
 
 }
