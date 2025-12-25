b9c2fbc8ed3bd8c13b987820dc037f28e92c0653
Cetin Erdem <e.cetinerdem@gmail.com>
2025-12-25 08:45:55 +0000

    17_04

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,9 +3,15 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        var request = new WithdrawalRequest(20, WithdrawalRequest.Currency.USD);
-        var atm = new UsDollarATM();
+        var request = new WithdrawalRequest(20, WithdrawalRequest.Currency.EUR);
+        var atm = buildAtmChain();
         atm.dispense(request);
+    }
+
+    private static ATM buildAtmChain() {
+        var euroATM = new EuroATM(null);
+        var usdATM = new UsDollarATM(euroATM);
+        return usdATM;
     }
 
 }
--- src/main/java/com/example/UsDollarATM.java
+++ src/main/java/com/example/UsDollarATM.java
@@ -2,10 +2,19 @@ package com.example;
 
 public class UsDollarATM extends ATM {
 
-    @Override
-    public void dispense(WithdrawalRequest request) {
-        System.out.println("Dispensing $" + request.getAmount());
+    public UsDollarATM(ATM nexAtm) {
+        super(nexAtm);
     }
 
+    @Override
+    public void dispense(WithdrawalRequest request) {
+
+        if (request.getCurrency().equals(WithdrawalRequest.Currency.USD)) {
+            System.out.println("Dispensing $" + request.getAmount());
+        } else if (nextAtm != null) {
+            nextAtm.dispense(request);
+        }
+        
+    }
 
 }
